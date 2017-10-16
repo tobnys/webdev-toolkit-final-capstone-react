aadc3e52b6f7090d8e5d2da44abf44a007c8b258
tobnys <tobnystrom@gmail.com>
2017-10-16 18:37:21 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,18 @@
+# Webdev Toolkit
+[![Build Status](https://travis-ci.org/tobnys/webdev-toolkit-final-capstone.svg?branch=master)](https://travis-ci.org/tobnys/webdev-toolkit-final-capstone)
+
+Link to live application: https://webdev-toolkit.herokuapp.com/
+
+# Screenshots
+
+# Goal
+
+# Use Case
+
+# Initial UX
+
+# Functionality
+
+# Technical stack
+
+
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+/*webpack-config.js*/
+var path = require('path');
+
+var webpack = require('webpack');
+
+var packageData = require('./package.json');
+
+var filename = [packageData.name, packageData.version, 'js'];
+
+module.exports = {
+    entry: path.resolve(__dirname, 'js/index.js'),
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'index.js',
+    },
+    devtool: 'source-map',
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+};
